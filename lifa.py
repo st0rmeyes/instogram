dcea299c06bf49b014a774a5ee8c7189008911ae
st0rmeyes <92102598+st0rmeyes@users.noreply.github.com>
2021-10-11 20:15:11 +0300

    Delete lifa.py

--- lifa.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-# help(smtplib)
-smtpObj = smtplib.SMTP('smtp.yandex.ru', 587)
-smtpObj.starttls()
-smtpObj.login('asonmbxnzmcb13377331@yandex.ru', 'gjxnfgjxnf2003')
-smtpObj.sendmail("asodfksdiojfoisdjfoi13377331@yandex.ru", "Остановись, брат")
-smtpObj.quit()
